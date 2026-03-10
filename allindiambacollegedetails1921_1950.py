4d50a5a7a81614b16a0fefb17b253f0d953acecc
cvm8173-hub <cvm8173@gmail.com>
2026-03-10 13:14:17 +0530

    Update allindiambacollegedetails1921_1950.py

--- allindiambacollegedetails1921_1950.py
+++ allindiambacollegedetails1921_1950.py
@@ -8464,11 +8464,8 @@ def parse_faculty_full_html(driver,URLS):
     driver.get(URLS["faculty"])
     wait = WebDriverWait(driver, 15)
 
-    # section = wait.until(
-    #     EC.presence_of_element_located(
-    #         (By.CSS_SELECTOR, "div.wikkiContents.faqAccordian")
-    #     )
-    # )
+    section = None
+    
     try:
         section = wait.until(
             EC.presence_of_element_located(
@@ -8476,19 +8473,19 @@ def parse_faculty_full_html(driver,URLS):
             )
         )
     except:
-        print("⚠️ parse_faculty_full_html not available, skipping")
-        return None
-
-    # 🔥 Scroll for lazy content
-    driver.execute_script(
-        "arguments[0].scrollIntoView({block:'center'});", section
-    )
-    time.sleep(2)
-
-    html = driver.execute_script(
-        "return arguments[0].innerHTML;", section
-    )
-
+        print("⚠️ parse_review_summarisation_all_tabs not available, skipping")
+    
+    if section:
+        driver.execute_script(
+            "arguments[0].scrollIntoView({block:'center'});", section
+        )
+        time.sleep(2)
+    
+        html = driver.execute_script(
+            "return arguments[0].innerHTML;", section
+        )
+    else:
+        html = None
     soup = BeautifulSoup(html, "html.parser")
 
     data = {
